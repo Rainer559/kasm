12106a31280d048b3a3d63aeb258500b2ebeaff4
Rainer559 <124501141+Rainer559@users.noreply.github.com>
2024-02-13 15:20:01 +0100

    Update next.config.js
    Anpassung

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'TIK-KASM',
+    description: 'TIK-Workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.com/Rainer559/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
